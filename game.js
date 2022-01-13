affacec8f4de0da6eb8f09928a3abc5ce60dd9b8
Carolus11145 <blumkarl221@gmail.com>
2022-01-13 09:54:11 +0200

    game.js

--- game.js
+++ game.js
@@ -1,12 +1,10 @@
-let option = ['rock', 'paper', 'scissors'];
-
 /* key to generating a random array */
 function computerPlay() {
+    let option = ['rock', 'paper', 'scissors'];
     let play = option[Math.floor(Math.random() * option.length)];
     return play;
-}
+};
 alert(computerPlay());
-
 
 function roundI (playerSelection, computerSelection) {
     playerSelection = prompt('Enter rock. paper or scissors: '); 
