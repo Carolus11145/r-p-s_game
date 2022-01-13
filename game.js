0a6f85d5ca51239c63c84f9b1208fb58f76e8dec
Carolus11145 <blumkarl221@gmail.com>
2022-01-13 10:01:33 +0200

    game.js

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
+let option = ['rock', 'paper', 'scissors'];
+
 /* key to generating a random array */
 function computerPlay() {
-    let option = ['rock', 'paper', 'scissors'];
     let play = option[Math.floor(Math.random() * option.length)];
     return play;
 };
-alert(computerPlay());
 
 function roundI (playerSelection, computerSelection) {
-    playerSelection = prompt('Enter rock. paper or scissors: '); 
+    playerSelection = prompt('Enter rock, paper or scissors: '); 
     computerSelection = computerPlay();
-    if (playerSelection == 'rock' && computerSelection == 'paper') {
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
         return 'You lose. Paper beats rock.';
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         return 'You win. Paper beats rock.';
     }
 };
